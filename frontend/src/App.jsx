b4021225a6cea039ab64d9c9b2a34cc866d44c45
noload <vaibhavkamble023@gmail.com>
2024-01-03 15:10:57 +0530

    todp app with frontend and backend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,22 +4,18 @@ import { CreateTodo } from './components/CreateTodo'
 import { Todos } from './components/Todos'
 
 function App() {
-  const [todos, setTodos] = useState(0)
+  const [todos, setTodos] = useState([])
   
-    // fetch('http://localhost:3000/todos').then(async function (res) {
-    //   const json = await res.json();
-    //   setTodos(json.todos)
-    //   console.log(json.todos);
-    // })
+    fetch('http://localhost:3000/todos').then(async function (res) {
+      const json = await res.json();
+      setTodos(json.todos);
+    })
 
   return (
     <>
      <div>
         <CreateTodo />
-        <Todos todos={[{
-          title:"go somewhere",
-          description:"i am going"
-        }]} ></Todos>
+        <Todos todos={todos} />
       </div> 
       
     </>
