10d0e9646d27caefc83a45387688d4d89835d691
noload <vaibhavkamble023@gmail.com>
2024-01-03 10:39:50 +0530

    created create and update endpoint

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,36 @@
 const express = require("express");
+const { createTodo, updateTodo } = require("./types");
 
 const app = express();
 const PORT = 3000;
 
 app.use(express.json());
 
-app.get("/todos", (res, res) => {});
+app.get("/todos", (req, res) => {});
 
-app.post("/todo", (req, res) => {});
+app.post("/todo", (req, res) => {
+  const createPayload = req.body;
+  const parsedPayload = createTodo.safeParse(createPayload);
 
-app.put("/completed", (req, res) => {});
+  if (!parsedPayload.success) {
+    res.status(411).json({
+      msg: "you sent the wrong input",
+    });
+    return;
+  }
+});
+
+app.put("/completed", (req, res) => {
+  const updatePayload = req.body;
+  const parsedPayload = updateTodo.safeParse(updatePayload);
+
+  if (!parsedPayload.success) {
+    res.status(411).json({
+      msg: "you sent the wrong input",
+    });
+    return;
+  }
+});
 
 app.listen(PORT, () => {
   console.log("serted started at " + PORT);
